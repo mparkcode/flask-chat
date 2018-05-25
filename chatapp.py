570976ffea5dbf17f9c4bee2b41ee21253539817
Michael Park <mparkcode@gmail.com>
2018-05-25 11:55:26 +0000

    saved chatapp.py

--- chatapp.py
+++ chatapp.py
@@ -38,5 +38,7 @@ def add_message(username):
     }
     messages.append(message)
     return redirect(username)
+
     
-app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')),debug = True)
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
